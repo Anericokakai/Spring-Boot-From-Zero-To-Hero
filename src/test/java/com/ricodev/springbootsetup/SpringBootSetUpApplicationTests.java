92c4574849b25f008a9924d3f9f3dd206a579b7c
Anericokakai <challykiddoh@gmail.com>
2024-06-01 19:08:02 +0300

    fix the initialisation of Spring

---
+++ src/test/java/com/ricodev/springbootsetup/SpringBootSetUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricodev.springbootsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSetUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
